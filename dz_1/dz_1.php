0a2bdf00b224e2fcc31b7c3ce5d09b9e0e5173c9
Stanislav <9226303141@mail.ru>
2022-12-12 20:50:34 +0500

    fix some mistakes from teacher

--- dz_1/dz_1.php
+++ dz_1/dz_1.php
@@ -1,16 +1,27 @@
 <?php
+
 $name = readline("Как Вас зовут?\n");
 $age = readline("Сколько Вам лет?\n");
+
 echo "Вас зовут $name, вам $age лет\n";
 
-$taskFirst = readline("Какая задача стоит перед вами сегодня?\n");
-$timeFirst = readline("Сколько примерно времени эта задача займет\n");
-$taskSecond = readline("Какая еще задача стоит перед вами сегодня?\n");
-$timeSecond = readline("Сколько примерно времени эта задача займет\n");
-$taskThird = readline("Какая еще задача стоит перед вами сегодня?\n");
-$timeThird = readline("Сколько примерно времени эта задача займет\n");
+
+$question = "Какая задача стоит перед вами сегодня?\n";
+$time = "Сколько примерно времени эта задача займет?\n";
+
+$taskFirst = readline("$question");
+$timeFirst = (int)readline("$time");
+
+$taskSecond = readline("$question");
+$timeSecond = (int)readline("$time");
+
+$taskThird = readline("$question");
+$timeThird = (int)readline("$time");
+
 $fullTime = $timeFirst + $timeSecond + $timeThird;
+
 echo "$name, сегодня у вас запланировано 3 приоритетных задачи на день:\n
-- $taskFirst ($timeFirst);\n - $taskSecond ($timeSecond);\n - $taskThird ($timeThird);\n
-Примерное время выполнения плана = $fullTime ч"
-?>
+- $taskFirst ({$timeFirst}ч);
+- $taskSecond ({$timeSecond}ч);
+- $taskThird ({$timeThird}ч);
+Примерное время выполнения плана = {$fullTime}ч";
